7202867617dcf5d36c618e8d830a9231c9fed866
DANIELS RAIVO IVANOVS 211RMB021 <118028746+DRIYSL@users.noreply.github.com>
2023-03-19 03:53:52 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -6,23 +6,8 @@ def build_heap(data):
     size = len(data)
     
     for i in range(size // 2, -1, -1):
-        while True:
-            min_element = i
-            left_child = 2*i + 1
-            right_child = 2*i + 2
+       
             
-            if left_child < size and data[left_child] < data[min_element]:
-                min_element = left_child
-            if right_child < size and data[right_child] < data[min_element]:
-                min_element = right_child
-                
-            if i == min_element:
-                break
-                
-            swaps.append((i, min_element))
-            data[i], data[min_element] = data[min_element],data[i]
-            
-            i = min_element
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
     return swaps
@@ -32,29 +17,21 @@ def main():
     # TODO : add input and corresponding checks
     # add another input for I or F 
     # first two tests are from keyboard, third test is from a file
+    
     # input from keyboard
-    n = int(input().strip())
+    n = int(input())
     data = list(map(int, input().split()))
     
 
     # checks if lenght of data is the same as the said lenght
     assert len(data) == n
     
-    mode = input().strip()
-    assert mode == 'I' or mode == 'F'
     
-    swaps = []
-    if mode == 'I':
-        swaps = build_heap(data)
-    elif mode == 'F':  
-        with open('swap.txt', 'r') as file:
-            for line in file:
-                swaps.append(tuple(map(int, line.split())))
-        
-    valid = all(data[i] <= data[2*i +1] and data[i] <= data[2*i +2] for i in range (n//2))
 
     # calls function to assess the data 
     # and give back all swaps
+    
+    swaps = build_heap(data)
 
     # TODO: output how many swaps were made, 
     # this number should be less than 4n (less than 4*len(data))
@@ -62,8 +39,8 @@ def main():
 
     # output all swaps
     print(len(swaps))
-    for i in swaps:
-        print(i[0], i[1])
+    for i, j in swaps:
+        print(i, j)
 
 
 if __name__ == "__main__":
